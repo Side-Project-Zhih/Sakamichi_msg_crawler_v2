9077fcb1326fdbb287299eb58663414ab4203d1f
zhih <fufong79570@gmail.com>
2022-06-19 03:22:26 +0800

    Modify ICommand method input type of receiver to AbstractGroupFactory & Set interface IDatabase

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -1,7 +1,17 @@
-import {} from "../type/type";
+import { TMember, TMessage } from "../type/type";
+import { AbstractGroupFactory } from "../groupFactory/AbstractGroupFactory";
 interface ICommand {
   execute(): Promise<void>;
-  setReceiver?(receiver: any): void;
+  setReceiver?(receiver: AbstractGroupFactory): void;
 }
 
-export { ICommand };
+interface IDatabase {
+  init(): Promise<void>;
+  storeMemberList(members: Array<TMember>): Promise<void>;
+  bulkStoreMsg(messages: Array<TMessage>): Promise<void>;
+  getMemberList(): Promise<Array<TMember>>;
+  getMembersInfo(members: Array<string>): Promise<Array<TMember>>;
+  checkMemberList(): Promise<boolean>;
+}
+
+export { ICommand, IDatabase };
