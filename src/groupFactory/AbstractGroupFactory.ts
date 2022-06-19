45c8423822e0337d1eb10e7813105aa82512293f
zhih <fufong79570@gmail.com>
2022-06-19 19:26:50 +0800

    Implemet method getMultiMemberMsg & getMemberList

--- src/groupFactory/AbstractGroupFactory.ts
+++ src/groupFactory/AbstractGroupFactory.ts
@@ -1,51 +1,175 @@
 import axios, { AxiosResponse } from "axios";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+
 import { Invoker } from "../invoker/Invoker";
 import { Api } from "../api/Api";
 import { IDatabase } from "../interface/interface";
+import { TMessage, TResMessage, TDownloadItem } from "../type/type";
+import { CommandDownloadItem } from "../command/CommandDownloadItem";
+import mkdirp from "mkdirp";
 
+const FILE_DATE_FORMAT = "YYYYMMDD-HHmmss";
+const DATE_FORMAT = "YYYYMMDDHHmmss";
 
 abstract class AbstractGroupFactory {
-  private accessToken?: string;
-  constructor(
-    private api: Api,
-    private db: IDatabase,
-    private refreshToken: string
-  ) {}
+  protected abstract api: Api;
+  protected abstract accessToken?: string;
+  protected abstract group: string;
+  constructor(protected db: IDatabase) {}
 
   async getMultiMemberMsg(
-    group: string,
     members: Array<string>,
-    starDate: string,
-    endDate: string
-  ) {}
+    startDate: string | void,
+    endDate: string | void
+  ) {
+    const group = this.group;
+    await this.db.init();
+
+    const isPass = await this.db.checkMemberList(group);
+    if (!isPass) {
+      await this.getAuth();
+      const api = this.api.GET_MEMBER_LIST;
+      const headers = this.api.getRequestHeader(this.accessToken);
+      const res: AxiosResponse = await axios.get(api, { headers });
+      let data = res.data as { [prop: number | string]: string }[];
+
+      const list = data.map((item) => ({
+        member_id: "" + item.id,
+        name: item.name,
+        group,
+      }));
+
+      await this.db.storeMemberList(list);
+    }
+
+    const membersInfo = await this.db.getMembersInfo(group, members);
+
+    for (const memberId of members) {
+      const member = membersInfo.find((item) => item.member_id === memberId);
+      if (!member) {
+        continue;
+      }
+
+      startDate = member.last_updated
+        ? dayjs.utc(member.last_updated).toISOString()
+        : dayjs.utc("20120101").toISOString();
+      endDate = dayjs.utc(new Date()).toISOString();
+
+      const dir = `${group}/${member.name}`;
+      const event = `${dir} downloading`;
+
+      await mkdirp(`${process.cwd()}/public/${dir}`);
+
+      //============================================================================
+      console.time(event);
+
+      const downLoadInvoker = new Invoker();
+      const storeMessages: Array<TMessage> = [];
+
+      await this.getAuth();
+      const messages = await this.fetchMsg(memberId, startDate, endDate, []);
+
+      for (const message of messages) {
+        const { type, text, file, published_at, updated_at } = message;
+
+        const storeObj: TMessage = {
+          member_id: memberId,
+          group,
+          type,
+          text,
+          published_at: dayjs.utc(published_at).format(DATE_FORMAT),
+          updated_at: dayjs.utc(updated_at).format(DATE_FORMAT),
+        };
+
+        const date = dayjs.utc(published_at).format(FILE_DATE_FORMAT);
+
+        if (type !== "text") {
+          let fileExtension: string = "";
+          const filename: string = `${date}-${type}`;
+
+          const downloadObj: TDownloadItem = {
+            link: file,
+            filename,
+            dir,
+            fileExtension,
+            type,
+          };
+
+          switch (type) {
+            case "picture": {
+              fileExtension = "jpg";
+              break;
+            }
+            case "voice": {
+              fileExtension = "mp4";
+              break;
+            }
+            case "video": {
+              fileExtension = "mp4";
+              break;
+            }
+            case "text": {
+              break;
+            }
+          }
+
+          downloadObj.fileExtension = fileExtension;
+
+          storeObj.dir = dir;
+          storeObj.file = `${filename}.${fileExtension}`;
+          downLoadInvoker.setCommand(new CommandDownloadItem(downloadObj));
+        }
+
+        storeMessages.push(storeObj);
+      }
+
+      if (storeMessages.length > 0) {
+        await this.db.bulkStoreMsg(storeMessages);
+      }
+
+      await downLoadInvoker.execute();
+
+      const last_updated = dayjs.utc(endDate).format(DATE_FORMAT);
+      await this.db.updateMemberLastUpdate(group, memberId, last_updated);
+      console.timeEnd(event);
+      //============================================================================
+      console.log(`${dir} download finish`);
+    }
+  }
 
   async getMemberList() {
+    const group = this.group;
     await this.db.init();
-    const isExist = await this.db.checkMemberList();
+    const isExist = await this.db.checkMemberList(group);
     if (!isExist) {
-      const headers = {};
-      const data = ((await axios.get("", { headers })) as AxiosResponse).data;
-      //TODO: 整理資料
-      // .....
-      const list: Array<object> = [];
-      //
+      await this.getAuth();
+      const apiUrl = this.api.GET_MEMBER_LIST;
+      const headers = this.api.getRequestHeader(this.accessToken);
+      const res: AxiosResponse = await axios.get(apiUrl, { headers });
+      let data = res.data as { [prop: number | string]: string }[];
+
+      const list = data.map((item) => ({
+        member_id: "" + item.id,
+        name: item.name,
+        group,
+      }));
+
       await this.db.storeMemberList(list);
       return list;
     }
-    const list = await this.db.getMemberList();
+    const list = await this.db.getMemberList(group);
 
     return list;
   }
 
-  async getMemberInfo(members: Array<string>) {
-    await this.db.init();
-  }
-
   async getSpecTimeMsg(
-     group: string,
-     member: string,
-     starDate: string,
-     endDate: string
+    group: string,
+    member: string,
+    starDate: string,
+    endDate: string
   ) {}
 
   async downloadSpecTimeItems(starDate: "string", endDate: string) {}
@@ -53,13 +177,21 @@ abstract class AbstractGroupFactory {
   getInvoker() {
     return new Invoker(this);
   }
+
+  protected async getMemberInfo(members: Array<string>) {
+    const group = this.group;
+
+    const memberInfo = await this.db.getMembersInfo(group, members);
+    return memberInfo;
+  }
+
   protected abstract getAuth(): Promise<string>;
   protected abstract fetchMsg(
     memberId: string,
     starDate: string,
     endDate: string,
-    msgContainer: Array<any>
-  ): Promise<Array<any>>;
+    msgContainer: Array<TResMessage>
+  ): Promise<Array<TResMessage>>;
 }
 
 export { AbstractGroupFactory };
