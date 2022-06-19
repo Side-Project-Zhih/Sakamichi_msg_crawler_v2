9d5264e276a92cc05ce481517d839d433503ac07
zhih <fufong79570@gmail.com>
2022-06-19 19:23:47 +0800

    Set nogizaka factory

---
+++ src/groupFactory/NogiFactory.ts
@@ -0,0 +1,63 @@
+import { AbstractGroupFactory } from "./AbstractGroupFactory";
+import { NogiApi } from "../api/NogiApi";
+import { IDatabase } from "../interface/interface";
+import axios, { AxiosResponse } from "axios";
+import { TResMessage } from "../type/type";
+
+const LIMIT = "200";
+
+class NogiFactory extends AbstractGroupFactory {
+  public api: NogiApi = new NogiApi();
+  public accessToken?: string;
+  public group: string = "nogi";
+  constructor(private refreshToken: string, db: IDatabase) {
+    super(db);
+  }
+
+  async getAuth() {
+    const payload = { refresh_token: this.refreshToken };
+    const headers = this.api.getRequestHeader();
+    const res: AxiosResponse = await axios.post(
+      this.api.GET_UPDATE_TOKEN,
+      payload,
+      {
+        headers,
+      }
+    );
+
+    const { access_token } = res.data;
+    this.accessToken = access_token;
+    return access_token;
+  }
+
+  async fetchMsg(
+    memberId: string,
+    starDate: string,
+    endDate: string,
+    msgContainer: Array<TResMessage>
+  ) {
+    const apiUrl = this.api.getMsgApi(memberId, {
+      count: LIMIT,
+      order: "asc",
+      created_from: starDate,
+      create_to: endDate,
+    });
+    const headers = this.api.getRequestHeader(this.accessToken);
+    const res: AxiosResponse = await axios(apiUrl, { headers });
+    const { messages, continuation } = res.data;
+
+    msgContainer.push(...messages);
+
+    if (continuation) {
+      const continuationInfo = JSON.parse(
+        Buffer.from(continuation, "base64").toString("ascii")
+      );
+      const newStarDate = continuationInfo.created_from;
+      await this.fetchMsg(memberId, newStarDate, endDate, msgContainer);
+    }
+
+    return msgContainer;
+  }
+}
+
+export { NogiFactory };
